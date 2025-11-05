d67b1f6a0ddea495d1d1a54d512492e6f924190b
kunaldaharwal.h <kunaldaharwal.h@gmail.com>
2025-11-05 06:16:48 +0000

    baneer_fix

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,13 @@
+
+'use client';
+
 import { SignIn } from "@clerk/nextjs";
 import { motion } from "framer-motion";
  
 export default function Page() {
   return (
     <div style={{
-      backgroundImage: "url('https://placehold.co/1920x1080')",
+      backgroundImage: "url('https://res.cloudinary.com/dcamqqj3j/image/upload/v1762322564/Login_tasknity.png')",
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       width: '100vw',
@@ -15,7 +18,13 @@ export default function Page() {
       paddingBottom: '5rem',
     }}>
       <motion.div whileTap={{ scale: 0.95 }}>
-        <SignIn />
+        <SignIn appearance={{
+          elements: {
+            card: "shadow-none bg-transparent",
+            socialButtonsBlockButton: "bg-slate-800 hover:bg-slate-700",
+            footerActionLink: "text-white hover:text-gray-200"
+          }
+        }} />
       </motion.div>
     </div>
   );
