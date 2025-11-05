1ad8d5a81c3f899e470af8c15b19b93e2f4ae640
kunaldaharwal.h <kunaldaharwal.h@gmail.com>
2025-11-05 06:11:31 +0000

    Login_banner

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,22 @@
 import { SignIn } from "@clerk/nextjs";
+import { motion } from "framer-motion";
  
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div style={{
+      backgroundImage: "url('https://placehold.co/1920x1080')",
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      paddingBottom: '5rem',
+    }}>
+      <motion.div whileTap={{ scale: 0.95 }}>
+        <SignIn />
+      </motion.div>
+    </div>
+  );
 }
